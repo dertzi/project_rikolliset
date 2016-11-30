79fad0b759c4542e2508920b39ea4da8e0e44195
Noctifera <noctifera@hotmail.fi>
2016-11-30 12:31:17 +0200

    Kohteiden alku

---
+++ src/model/Kohteet.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Hanne
+ */
+public class Kohteet {
+    
+    private String nimi, turvallisuus;
+    private double cpDouble, random;
+    private int cp, vaikeustaso;
+    
+    public void lisääKohde(int vaikeustaso) {
+        this.vaikeustaso = vaikeustaso;
+    }
+    
+    public double getCp() {
+        switch (vaikeustaso) {
+            case 1:
+                cpDouble = laskeRandom(1.0, 10.0);
+                break;
+            case 2:
+                cpDouble = laskeRandom(11.0, 20.0);
+                break;
+            case 3:
+                cpDouble = laskeRandom(21.0, 30.0);
+                break;
+            case 4:
+                cpDouble = laskeRandom(31.0, 40.0);
+                break;
+            case 5:
+                cpDouble = laskeRandom(41.0, 50.0);
+                break;              
+        }
+        cp = (int)cpDouble;
+        return cp;
+    }
+    
+    public String getNimi() {
+        /*listasta        
+        */
+        return nimi;
+    }
+    
+    public String getTurvallisuus() {
+        /*listasta       
+        */
+        return turvallisuus;
+    }
+    
+    public double laskeRandom(double min, double max) {        
+        double minimi = min;
+        double maksimi = max;
+        random = Math.random()*(maksimi-minimi) + minimi;
+        return random;
+    }
+    
+    
+    public String getStats() {
+        return "Nimi: "+nimi+"CP: "+cp+"Vaikeustaso: "+vaikeustaso+"Turvallisuus: "+turvallisuus;
+    }
+            
+}

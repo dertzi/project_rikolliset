3104be189f9107c32a2a943ce991181869e943e7
dertzi <andre.derghokasian@gmail.com>
2016-12-10 15:45:27 +0200

    kontrolleri/käyttöliittymä
    käyttöliittymä nyt sisältää kaksi printti metodia:
    näytäln ja näytä. toinen tulostaa rivinvaihdolla ja toinen ilman.
    
    käyttöliittymän lueInt metodi nyt tarkistaa että arvona on varmasti
    kokonaisluku.
    
    Kontrolleri päivitetty vastaamaan käyttöliittymän päivitystä. Päivitetty
    kaupan toimintaa. Pitää lisätä vielä indexOutOfRange tarkistus.

--- src/Kontrolleri/Kontrolleri.java
+++ src/Kontrolleri/Kontrolleri.java
@@ -27,15 +27,15 @@ public class Kontrolleri {
         // Tarviiko tämän olla näin monimutkainen? :D
         // Miksi simppeli String ei kelpaa?
         final String[] valikko = {"\nToiminta vaihtoehdot", "1. Kohteet", "2. Rikollisisjengin jäsenet", "3. Kauppa",
-            "4. Poliisit", "5. Placeholder", "6. Lopeta simulaatio"};
+            "4. Poliisit", "5. Placeholder", "6. Lopeta simulaatio\n"};
         String arrayInString = Arrays.toString(valikko);
         String finalArray = arrayInString.replace(",", " | ");
         String finalArray1 = finalArray.replace("]", "");
         String finalArray2 = finalArray1.replace("[", "");
 
         /* Simulaation aloitusteksti */
-        UI.näytä("Tervetuloa |> RIKOLLISET <| simulaatiopeliin");
-        UI.näytä("\nTämä simulaatio simuloi rikollismaailman elämää." + "\nSimulaation käyttäjänä pääset ohjaamaan rikollisjengin ryöstöjä ja\n"
+        UI.näytäln("Tervetuloa |> RIKOLLISET <| simulaatiopeliin");
+        UI.näytäln("\nTämä simulaatio simuloi rikollismaailman elämää." + "\nSimulaation käyttäjänä pääset ohjaamaan rikollisjengin ryöstöjä ja\n"
                 + "hankkeita tielläsi alamaailman huipulle pahamaineisinpana rikollisorganisaation johtajana.");
 
         // FIX: muutetaan tämä booleaniks ja katsotaan jatketaanko luuppia jos true/false
@@ -43,18 +43,18 @@ public class Kontrolleri {
 
         // FIX: kyllä/ei tilanne turha? "paina enter aloittaaksesi".
         // hoidetaan pelin lopettaminen valikon kautta.
-        UI.näytä("\nAloitetaanko simulaatio? (k/e)");
+        UI.näytäln("\nAloitetaanko simulaatio? (k/e)");
         String valinta = UI.lueString();
 
         if (valinta.equals("k") | valinta.equals("K")) {
-            UI.näytä("Hienoa, lähdetään liikeelle.\n");
+            UI.näytäln("Hienoa, lähdetään liikeelle.\n");
 
         } else {
             /* Simulaation aloitus kysymys */
             int j = 0;
             do {
-                UI.näytä("\nVirheellinen syöte");
-                UI.näytä("\nAloitetaanko simulaatio? (k/e)\n");
+                UI.näytäln("\nVirheellinen syöte");
+                UI.näytäln("\nAloitetaanko simulaatio? (k/e)\n");
                 String syöte = UI.lueString();
                 if (syöte.equals("k") | syöte.equals("K")) {
                     j = 1;
@@ -66,28 +66,25 @@ public class Kontrolleri {
         /* Peruslooppi */
         do {
             // Tähän voisi vaikka tulla joitain perusstatseja, kuten jäsenien määrä tjms (iha printtinä)
-            UI.näytä(finalArray2);
+            UI.näytäln(finalArray2);
             UI.näytä("Valinta: ");
-            // FIX: LueInt metodilla ei ole syötteen virheentarkistusta.
-            // Joko LueInt pitää korjata tai sitten käytetään vain lueString ja
-            // arvoina "1", "2" jne. Tällä tavalla ei tarvitse pelätä että ohjelma
-            // kaatuu virheelliseen syötteeseen. switch default palauttaa siis "virheellinen syöttö".
+
             int valinta2 = UI.lueInt();
             switch (valinta2) {
                 case 1:
                     // Tulostaa kaikki kohteet
-                    UI.näytä("\nKohteet:\n");
-                    UI.näytä("Marketit\n");
+                    UI.näytäln("\nKohteet:\n");
+                    UI.näytäln("Marketit\n");
                     for (int j = 0; j < kohde.getMarkettiInt(); j++) {
-                        UI.näytä(j + ". " + kohde.getMarketti(j));
+                        UI.näytäln(j + ". " + kohde.getMarketti(j));
                     }
-                    UI.näytä("\nPankit\n");
+                    UI.näytäln("\nPankit\n");
                     for (int j = 0; j < kohde.getPankkiInt(); j++) {
-                        UI.näytä(j + ". " + kohde.getPankki(j));
+                        UI.näytäln(j + ". " + kohde.getPankki(j));
                     }
-                    UI.näytä("\nVankilat\n");
+                    UI.näytäln("\nVankilat\n");
                     for (int j = 0; j < kohde.getVankilaInt(); j++) {
-                        UI.näytä(j + ". " + kohde.getVankila(j));
+                        UI.näytäln(j + ". " + kohde.getVankila(j));
                     }
                     break;
                 case 2:
@@ -98,19 +95,25 @@ public class Kontrolleri {
 //                    }
                     break;
                 case 3:
+                    // TODO: Lisää tarkistus maksimi arvolle (index out of range!)
                     // Kauppa päivittyy kun yksi rikollinen on ostettu.
-                    UI.näytä(kauppa);
-                    UI.näytä("Osta rikollinen nro: ");
+                    UI.näytäln(kauppa);
+                    UI.näytäln("Kenet tahdot ostaa? (Poistu: 0)");
+                    UI.näytä("Valinta: ");
                     // FIX: Tähän tarvitsee virheen tarkistuksen siltä varalta että syöttää korkean arvon tai muuta kuin kokonaislukuja!
                     // FIX: Lisää kaupasta poistuminen!
                     int syöte = UI.lueInt();
-                    if (rikolliset.getRaha() >= kauppa.ostaRikollinen(syöte).getArvo()) {
+                    if (syöte == 0) {
+                        break;
+                    } else if (rikolliset.getRaha() >= kauppa.ostaRikollinen(syöte).getArvo()) {
                         rikolliset.lisääJäsen(kauppa.ostaRikollinen(syöte));
                         rikolliset.setRaha(rikolliset.getRaha() - kauppa.ostaRikollinen(syöte).getArvo());
                         kauppa.päivitäValikoima();
                     } else {
                         System.out.println("Ei riittävästi varaa");
                     }
+                    
+                    // Testi tulostus
                     System.out.println("Maine: " + rikolliset.getMaine() + "\nCP: " + rikolliset.getCombatPower() + "\nRahat: " + rikolliset.getRaha() + "\n------------\n");
                     // Kauppa, päivittyy jokaisella kerralla uuteen valikoimaan random arvoilla
                     // myös poistaa rikollisen kaupan listalta, mikäli käyttäjä ostaa rikollisen <--- miksi poistaa rikollinen listalta jos se tyhjennetään kuitenkin?
@@ -134,10 +137,10 @@ public class Kontrolleri {
 //                        }
                     break;
                 case 4:
-                    UI.näytä("Placeholder");
+                    UI.näytäln("Placeholder");
                     break;
                 case 0:
-                    UI.näytä("For testing purposes.");
+                    UI.näytäln("For testing purposes.");
                     break;
 
             }

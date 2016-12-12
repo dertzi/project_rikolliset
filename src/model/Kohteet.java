5422132b09343a7088921a93e56d214ccc01a378
dertzi <andre.derghokasian@gmail.com>
2016-12-12 03:55:03 +0200

    Paljon muutoksia!
    Jouduin muuttamaan aika paljon että saadaan noi kohteet toimimaan
    hyökkäystilanteessa oikein. En ihan vielä saanut kaikkea tehtyä, mutta
    kun kello on jo 3:52, tekis mieli mennä jo nukkumaan. Jatkan siis
    huomenna!
    
    Hommasta siis puuttuu enään muutama tarvittava funktio
    KohteetToiminnallisuuteen ja sitten kohdevankilalle ja kohteelle.
    Kontrolleriin pitää luoda hyökkäystoiminto ja poliisit ja poliisiasema
    puuttuu.
    
    En ole ehtinyt hirveästi kommentoimaan itse koodia, mikä aiheuttaa sen
    että saattaa olla hieman epäselvää mitä jossakin tapahtuu. Pahoittelen!
    :dancers:  Muutin myös kohteiden luomis prosessia ja tein hieman
    exponentiaalisemman kasvun kohteiden CPlle suhteessa vaikeustasoon.

--- src/model/Kohteet.java
+++ src/model/Kohteet.java
@@ -1,34 +1,37 @@
 package model;
 
 import java.util.ArrayList;
-
+import java.util.Random;
 public class Kohteet {
 
     private ArrayList<KohdeMarketti> marketit;
     private ArrayList<KohdePankki> pankit;
     private ArrayList<KohdeVankila> vankilat;
+    private Random random;
+    
+    private String[] nimetMarketti = {"K-market", "S-market", "Alepa", "Siwa", "Valintatalo", "R-kioski", "Lidl", "Apteekki", "Stockmann", "Tokmanni", "Prisma", "Citymarket",
+        "Verkkokauppa", "GameStop", "Ikea", "Motonet", "Kultasepän liike", "Stadium", "Gigantti", "Jysk", "Hong kong", "Arnolds", "McDonalds", "Hesburger", "BurgerKing", "Clas Ohlson",
+        "Nissen", "DNA kauppa", "Elisa kauppa", "Apple kauppa", "Intersport", "Sonera kauppa", "Kotipizza", "Teknikmagasinet"};
+    private String[] nimetPankit = {"S-pankki", "Danske Bank", "Suomen Pankki", "Helsingin Osakepankki", "Suomen yhdyspankki", "Luotto-Pankki", "Bigbank", "Interbank", "Mandatum",
+        "Liittopankki", "Liikepankki", "Sofia Pankki", "Svenska Handelsbanken", "Trevise Pankki", "Turun Osakepankki", "Uudenmaan Osakepankki", "Wasa Aktie Bank", "Ålandsbanken",
+        "Maakuntain Pankki", "Landtmannabanken", "Maakiinteistäpankki", "Savo-Karjalan Osake-Pankki", "Postipankki", "Atlas-Pankki", "Kansallis-Osake-Pankki"};    
+    private String[] nimetVankilat = {"Helsingin vankila", "Hämeenlinnan vankila", "Riihimäen vankila", "Kuopion vankila", "Mikkelin vankila", "Oulun vankila", "Jokelan vankila", "Keravan vankila",
+        "Vanajan vankila", "Helsingin avovankila", "Vantaan vankila", "Kylmäkosken vankila", "Käyrän vankila", "Satakunnan vankila", "Turun vankila", "Vaasan vankila", "Vilppulan vankila",
+        "Juuan vankila", "Laukaan vankila", "Pyhäselän vankila", "Sukevan vankila", "Ylitornion vankila"};
+
     private final int MAKSIMI_PER_KOHDE = 5;
 
     // kohteiden luominen (konstruktori)
     public Kohteet() {
+        random = new Random();
         marketit = new ArrayList<>();
         pankit = new ArrayList<>();
         vankilat = new ArrayList<>();
 
         for (int i = 0; i < MAKSIMI_PER_KOHDE; i++) {
-            marketit.add(new KohdeMarketti());
-            pankit.add(new KohdePankki());
-            vankilat.add(new KohdeVankila());
-        }
-        for (int k = 0; k < MAKSIMI_PER_KOHDE; k++) {
-            marketit.get(k).setNimiMarketti();
-            marketit.get(k).setCP();
-
-            pankit.get(k).setNimiPankki();
-            pankit.get(k).setCP();
-
-            vankilat.get(k).setNimiVankila();
-            vankilat.get(k).setCP();
+            marketit.add(new KohdeMarketti(nimetMarketti[random.nextInt(nimetMarketti.length)], i));
+            pankit.add(new KohdePankki(nimetPankit[random.nextInt(nimetPankit.length)], i));
+            vankilat.add(new KohdeVankila(nimetVankilat[random.nextInt(nimetVankilat.length)], i));
         }
     }
 
@@ -36,6 +39,19 @@ public class Kohteet {
         return MAKSIMI_PER_KOHDE;
     }
 
+    public ArrayList<KohdeMarketti> getMarketit() {
+        return marketit;
+    }
+
+    public ArrayList<KohdePankki> getPankit() {
+        return pankit;
+    }
+
+    public ArrayList<KohdeVankila> getVankilat() {
+        return vankilat;
+    }
+    
+    
     public String getMarketitString() {
         String merkkijono = "";
         for (int i = 0; i < marketit.size(); i++) {
@@ -85,5 +101,4 @@ public class Kohteet {
         }
         return merkkijono;
     }
-
 }
